7243c1d5bf28bb64416086b9802b84b49d5d144f
dykdykdyk <1825041608@qq.com>
2017-03-22 16:17:51 +0800

    first command

--- Eventdistribution/src/com/example/eventdistribution/MainActivity.java
+++ Eventdistribution/src/com/example/eventdistribution/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+	 public static final String TAG = "TouchDemoActivity";  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
