2606c8aebb8115a923fedd6a5bce2158eac8f7e1
dykdykdyk <1825041608@qq.com>
2017-04-26 18:33:11 +0800

    zklg

--- ViewPagerDemo/app/src/main/java/dyk/viewpagerdemo/SplashActivity.java
+++ ViewPagerDemo/app/src/main/java/dyk/viewpagerdemo/SplashActivity.java
@@ -1,26 +1,38 @@
 package dyk.viewpagerdemo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-/**
+/**启动 动画
  * Created by dengyangkang on 2017/4/24.
  */
 
-public class SplashActivity extends Activity{
+public class SplashActivity extends FragmentActivity {
     Animation scaleAnimation;
     ImageView mImageView;
     LinearLayout splsh_linearlayout ;
+    FragmentManager manager;
+    Guider_Fragment fragment1;
+    FragmentTransaction transaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
         setContentView(R.layout.splash_main);
+        manager = getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+        fragment1 = new Guider_Fragment();
         splsh_linearlayout =(LinearLayout)findViewById(R.id.splsh_linearlayout);
         mImageView =new ImageView(this);
         mImageView.setImageResource(R.mipmap.guide_1);
@@ -34,15 +46,16 @@ public class SplashActivity extends Activity{
                 mImageView.startAnimation(scaleAnimation);
                 splsh_linearlayout.addView(mImageView);
             }
-        }, 2000); // 启动动画持续2秒钟
+        }, 500); // 启动动画持续2秒钟
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                Intent intent = new Intent(SplashActivity.this,
-                       MainActivity.class); // 从启动动画ui跳转到主ui
+                       Guider_Fragment.class); // 从启动动画ui跳转到主ui
                 startActivity(intent);
+                overridePendingTransition(R.anim.out, R.anim.in);
              SplashActivity.this.finish();
             }
-        }, 3000); // 启动动画持续3秒钟
+        }, 1500); // 启动动画持续3秒钟
     }
 }
